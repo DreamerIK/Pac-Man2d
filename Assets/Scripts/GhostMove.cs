3994b6ccbc45179f380b2f65181e2315d8dd5ef9
Ikhtiar Karimov <karimov021298@gmail.com>
2019-05-24 23:11:06 +0600

    Default Pac-Man

---
+++ Assets/Scripts/GhostMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostMove : MonoBehaviour
+{
+    public Transform[] waypoints;//массив Transform, чтобы позже мы могли установить путевые точки в Инспекторе
+    int cur = 0;//индексная переменная, которая отслеживает точку пути, к которой в данный момент идет Blinky
+    public float speed = 0.3f;//скорость движения
+
+    void FixedUpdate()
+    {
+        // Waypoint not reached yet? then m
+        Vector2 myPos = transform.position;
+        Vector2 endPos = waypoints[cur].position;
+        if (myPos.ToString().Equals(endPos.ToString()))
+        {
+            cur = (cur + 1) % waypoints.Length;
+        } else
+        {
+            Vector2 p = Vector2.MoveTowards(transform.position, waypoints[cur].position, speed);
+            GetComponent<Rigidbody2D>().MovePosition(p);
+        }
+
+       
+
+        // Animation
+        Vector2 dir = waypoints[cur].position - transform.position;
+        GetComponent<Animator>().SetFloat("DirX", dir.x);
+        GetComponent<Animator>().SetFloat("DirY", dir.y);
+    }
+
+    void OnTriggerEnter2D(Collider2D co)//тут можно добавить ендгейм
+    {
+        if (co.name == "pacman")
+            Destroy(co.gameObject);
+    }
+}
---
+++ Assets/Scripts/Pacdot.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pacdot : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D co)//проверяем было ли столкновение с пакманом, если да, то уничтожаем
+    {//тут можно вести счет
+        if (co.name == "pacman")
+            Destroy(gameObject);
+    }
+}
